2112b27305d9e8f0489d8f8f668bd4d7c9fa2dd2
日下陸矢 <rikuya.98@gmail.com>
2025-04-07 21:13:32 +0900

    [add]プルリクエスト取り込みAPIの作成

---
+++ backend/api/app/Http/Controllers/GitHub/ImportPullRequestController.php
@@ -0,0 +1,52 @@
+<?php
+
+    namespace App\Http\Controllers\GitHub;
+
+    use App\Constants\ApiMessages;
+    use App\Contexts\GitHubApi\Exception\GitHubApiException;
+    use App\Contexts\GitHubApi\UseCase\ImportPullRequestsUseCase;
+    use App\Http\Controllers\Controller;
+    use Exception;
+    use Illuminate\Http\JsonResponse;
+    use Illuminate\Http\Request;
+
+    class ImportPullRequestController extends Controller
+    {
+        public function __construct(
+            private readonly ImportPullRequestsUseCase $useCase
+        )
+        {
+        }
+
+        /**
+         * GitHubのコラボレーター情報を取得・保存するAPI
+         *
+         * @param Request $request
+         * @return JsonResponse
+         */
+        public function __invoke(Request $request): JsonResponse
+        {
+            try {
+                $result = $this->useCase->handle(
+                    $request->route('owner'),
+                    $request->route('repository')
+                );
+
+                return response()->json([
+                    'success' => true,
+                    'message' => ApiMessages::PULL_REQUESTS_IMPORTED_SUCCESS,
+                    'total_count' => $result['total_count']
+                ]);
+            } catch (GitHubApiException $e) {
+                return response()->json([
+                    'success' => false,
+                    'message' => ApiMessages::GITHUB_API_ERROR_PREFIX . $e->getMessage(),
+                ], 500);
+            } catch (Exception $e) {
+                return response()->json([
+                    'success' => false,
+                    'message' => ApiMessages::GENERAL_ERROR_PREFIX . $e->getMessage(),
+                ], 500);
+            }
+        }
+    }
