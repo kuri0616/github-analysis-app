53112368bb1464cc4c68b51fa139928fc412bb9c
日下陸矢 <rikuya.98@gmail.com>
2025-04-07 21:12:25 +0900

    [add]エラーメッセージの追加

--- backend/api/app/Constants/ApiMessages.php
+++ backend/api/app/Constants/ApiMessages.php
@@ -1,14 +1,16 @@
 <?php
 
-namespace App\Constants;
+    namespace App\Constants;
 
-class ApiMessages
-{
-    // 成功メッセージ
-    public const COLLABORATORS_IMPORTED_SUCCESS = 'コラボレーター情報のインポートに成功しました';
+    class ApiMessages
+    {
+        // 成功メッセージ
+        public const  COLLABORATORS_IMPORTED_SUCCESS = 'コラボレーター情報のインポートに成功しました';
 
-    // エラーメッセージ
-    public const GITHUB_API_ERROR_PREFIX = 'GitHub APIでエラーが発生しました: ';
-    public const GENERAL_ERROR_PREFIX = '予期せぬエラーが発生しました: ';
+        public const  PULL_REQUESTS_IMPORTED_SUCCESS = 'プルリクエスト情報のインポートに成功しました';
 
-}
+        // エラーメッセージ
+        public const  GITHUB_API_ERROR_PREFIX = 'GitHub APIでエラーが発生しました: ';
+        public const  GENERAL_ERROR_PREFIX = '予期せぬエラーが発生しました: ';
+
+    }
